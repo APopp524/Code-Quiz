10a31cabf84ff9fe52bca0982f60fcea57850258
Andrew Popp <apopp524@gmail.com>
2020-07-01 19:30:22 -0500

    final changes

---
+++ questions.js
@@ -0,0 +1,28 @@
+
+    const questions = [
+        {
+            title: "Question 1: Why so JavaScript and Java have similar name?",
+            choices: ["JavaScript is a stripped-down version of Java", "JavaScript's syntax is loosely based on Java's", "They both originated on the island of Java", "None of the above"],
+            answer: "avaScript's syntax is loosely based on Java's"
+        },
+        {
+            title: "Question 2: ______ JavaScript is also called client-side JavaScript.",
+            choices: ["Microsoft", "Navigator", "LiveWire", "Native"],
+            answer: "Navigator"
+        },
+        {
+            title: "______ tag is an extension to HTML that can enclose any number of JavaScript",
+            choices: ["SCRIPT", "BODY", "HEAD", "TITLE"],
+            answer: "SCRIPT"
+        },
+        {
+            title: "Question 4: Which of the following is not considered a JavaScript operator?",
+            choices: ["new", "this", "delete", "typeof"],
+            answer: "this"
+        },
+        {
+            title: "Question 5: The _______ method of an Array object adds and/or removes elements from an array.",
+            choices: ["Reverse", "Slice", "Shift", "Splice"],
+            answer: "Splice"
+        }
+    ];
